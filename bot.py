a7d5f72c3b4738443fcc76c23cdc733afe28250a
FXCProject <futurexcoin2025@gmail.com>
2025-05-06 00:40:05 +0330

    Update bot.py
    from telegram import Update
    from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
    
    BOT_TOKEN = "7711233839:AAFnU89KQ8d2XTyOM3-_jCzDtvf0yd9JvAU"
    
    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
        await update.message.reply_text("Welcome to the Future X Airdrop!")
    
    if __name__ == "__main__":
        app = ApplicationBuilder().token(BOT_TOKEN).build()
        app.add_handler(CommandHandler("start", start))
        app.run_polling()

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Welcome to the Future X Airdrop!")
 
 if __name__ == "__main__":
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
