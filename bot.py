9273e4d650005dd54920c71467ec2d1d94489888
FXCProject <futurexcoin2025@gmail.com>
2025-05-06 01:06:39 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
- app.run_polling()
+app.run_polling()
